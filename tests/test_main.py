0d39c46933b7ae2b1fd6208b69ac2b3e404ae195
Stig B. Dørmænen <stigbd@gmail.com>
2024-10-28 17:42:27 +0100

    Use async test client

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,23 +1,34 @@
 """Test the main function."""
 
+import pytest
 from fastapi import status
-from fastapi.testclient import TestClient
+from httpx import ASGITransport, AsyncClient
 
 from app import app
 
 
-def test_get_root() -> None:
+@pytest.fixture
+def anyio_backend() -> str:
+    """Use the asyncio backend for the anyio fixture."""
+    return "asyncio"
+
+@pytest.mark.anyio
+async def test_get_root() -> None:
     """Should return 200 and a json body."""
-    client = TestClient(app)
-    response = client.get("/")
+    async with AsyncClient(
+            transport=ASGITransport(app=app), base_url="http://test"
+        ) as ac:
+            response = await ac.get("/")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"message": "Hello World"}
 
-
-def test_head_root() -> None:
+@pytest.mark.anyio
+async def test_head_root() -> None:
     """Should return 200 and an empty body."""
-    client = TestClient(app)
-    response = client.head("/")
+    async with AsyncClient(
+            transport=ASGITransport(app=app), base_url="http://test"
+        ) as ac:
+            response = await ac.head("/")
     assert response.status_code == status.HTTP_200_OK
     assert "content-type" not in response.headers
     assert response.headers["content-length"] == "0"
